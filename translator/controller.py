846db026675b52eccebdd191b807553626e87d03
Adarsh Singh <adarshvns29@gmail.com>
2022-10-10 02:49:30 +0530

    csv file reading and storing in db as base64

--- translator/controller.py
+++ translator/controller.py
@@ -1,7 +1,15 @@
-from operator import truediv
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse
+from translator.helper import (
+  save_file_as_base64, 
+  save_file_as_temp_csv
+)
 
-# Create your views here.
 def upload_csv(request):
-  pass
+  if request.method == 'POST':
+    file = request.FILES['file']
+    content = file.read()
+    file_id = save_file_as_base64(content)
+    csv_file = save_file_as_temp_csv(content)
+    
+  
+  return JsonResponse({'msg':'uploaded succesfully'})
--- translator/models.py
+++ translator/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class File(models.Model):
+  _id = models.ObjectIdField()
+  file_base64 = models.TextField()
